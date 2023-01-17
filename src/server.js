b070b3e33c86cd14bc3269ac3f91352d77ee9e9a
Alejo Martinez <alejoomartinex11@gmail.com>
2023-01-17 13:36:32 -0300

    empezando desafio handlebars

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import productsRouter from './routes/products.router.js'
 import cartRouter from './routes/cart.router.js'
 import {__dirname} from './utils.js'
+import handlebars from 'express-handlebars'
+import path from 'path'
 
 
 const app = express()
@@ -9,8 +11,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/static', express.static(__dirname + '/public'))
+// app.use('/static', express.static(__dirname +'/public'))
 
+app.engine('handlebars', handlebars.engine())
+
+app.set('views', __dirname+'views')
+app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.listen(3005, ()=>{
     console.log('server iniciado');
